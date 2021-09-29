63d758b38adb99cc187a7eb1fac412660d2d5663
Anika Choudhury <anikachoudhury010@gmail.com>
2021-09-29 17:19:04 +0100

    form completed

--- src/pages/add.js
+++ src/pages/add.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './add.css';
 
 function add() {
     return (
@@ -6,53 +7,53 @@ function add() {
             <h1>Add a new patient</h1>
 
             <form>
-                <label for="firstname">First Name:</label>
+                <label for="firstname">First Name:</label><br/>
                 <input type="text" id="firstname" name="firstname"></input><br/>
 
-                <label for="lastname">Last Name:</label>
+                <label for="lastname">Last Name:</label><br/>
                 <input type="text" id="lastname" name="lastname"></input><br/>
 
-                <label for="sex">Sex:</label>
+                <label for="sex">Sex:</label><br/>
                 <select id="sex" name="sex">
                     <option value="female">Female</option>
                     <option value="male">Male</option>
                     <option value="other">Other</option>
                 </select><br/>
 
-                <label for="dob">Date of Birth:</label>
+                <label for="dob">Date of Birth:</label><br/>
                 <input type="date" id="dob" name="dob"></input><br/>
 
-                <label for="age">Age:</label>
+                <label for="age">Age:</label><br/>
                 <input type="text" id="age" name="age"></input><br/>
 
                 <label for="admission">Reason For Admission:</label><br/>
                 <textarea id="admission" name="admission" rows="5" cols="50"></textarea><br/>
 
-                <label for="doa">Date of Admission:</label>
+                <label for="doa">Date of Admission:</label><br/>
                 <input type="date" id="doa" name="doa"></input><br/>
 
-                <label for="covid">Covid Risk:</label>
+                <label for="covid">Covid Risk:</label><br/>
                 <select id="covid" name="covid">
                     <option value="high">High</option>
                     <option value="low">Low</option>
                     <option value="mid">Mid</option>
                 </select><br/>
 
-                <label for="risk">Risk Assessment:</label>
+                <label for="risk">Risk Assessment:</label><br/>
                 <select id="risk" name="risk">
                     <option value="highrisk">High Risk</option>
                     <option value="lowrisk">Low Risk</option>
                     <option value="midrisk">Mid Risk</option>
                 </select><br/>
 
-                <label for="doctor">Assign a Doctor:</label>
+                <label for="doctor">Assign a Doctor:</label><br/>
                 <select id="doctor" name="doctor">
                     <option value="doc1">doc 1</option>
                     <option value="doc2">doc 2</option>
                     <option value="doc3">doc 3</option>
                 </select><br/>
 
-                <label for="ward">Assign a Ward:</label>
+                <label for="ward">Assign a Ward:</label><br/>
                 <select id="ward" name="ward">
                     <option value="ward1">ward 1</option>
                     <option value="ward2">ward 2</option>
